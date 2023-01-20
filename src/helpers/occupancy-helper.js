3e3af26f7c4615a5b5c673d54f10cc8ec68568b9
John Nelson <automationaddict@me.com>
2023-01-19 23:00:57 -0500

    Add JSDoc, ReadConfig function
    Add JSDoc. Add ReadConfig function to check input payload. Add src/ pattern so helper code is included.

--- src/helpers/occupancy-helper.js
+++ src/helpers/occupancy-helper.js
@@ -8,7 +8,7 @@ function fetchConfig (path) {
       try {
         return yaml.load(fs.readFileSync(path, 'utf8'), {})
       } catch (e) {
-        throw Error(e)
+        throw Error(e) // YAML Error
       }
     } else {
       throw new Error('File does not exist')
@@ -18,4 +18,27 @@ function fetchConfig (path) {
   }
 }
 
+function ReadConfig (msg, fetchConfigCallback) {
+  /** @namespace data.inputArgs */
+  this.payload = msg.payload
+  this.control = msg.control // Add control to msg Object
+  this.type = msg.type // Add type to msg Object
+  this.data = fetchConfigCallback()
+  this.validKey = function () {
+    if (this.type === 'inputArgs') {
+      return Object.prototype.hasOwnProperty.call(this.data[this.type], this.payload)
+    } else if (this.type === 'controlArgs') {
+      return this.data[this.type][this.control].includes(msg.payload)
+    }
+  }
+  this.getValue = function () {
+    if (this.validKey()) {
+      return this.data.inputArgs[this.payload]
+    } else {
+      throw new Error(`Payload: ${this.payload} is invalid!`)
+    }
+  }
+}
+
 module.exports.fetchConfig = fetchConfig
+module.exports.readConfig = ReadConfig
